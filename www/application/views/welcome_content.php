7e184517cd4532075b0a5c68ff5e25928cc05ca5
Chris Hardcastle <me@chrishardcastle.co.uk>
2010-04-03 22:42:27 +0100

    Changed js to use the more flexible HTML of pagination via kohana, rather than generating the links in js.

--- www/application/views/welcome_content.php
+++ www/application/views/welcome_content.php
@@ -1,19 +1,20 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
 
-<?php
-// pagination 
-echo $hotlinks;
-?>
-
-<?php foreach($posts as $key => $post){ ?>
-	<div id="box<?php echo $key+1; ?>" class="outer">
-		<a href="#" class="close ui-icon ui-icon-close" style="float:right">Close</a>		
-		<div class="pubDate"><?php echo $post->date; ?></div>
-		<?php 
-		$features = json_decode($post->content); 
-		foreach($features as $f){
-		?>
-			<div><?php echo stripslashes($f); ?></div>
-		<?php } ?>				
-	</div>
-<?php } ?>
+	<?php
+		// pagination 
+		echo $hotlinks;
+	?>
+<div id="container">
+	<?php foreach($posts as $key => $post){ ?>
+		<div id="box<?php echo $key+1; ?>" class="outer">
+			<a href="#" class="close ui-icon ui-icon-close" style="float:right">Close</a>		
+			<div class="pubDate"><?php echo $post->date; ?></div>
+			<?php 
+			$features = json_decode($post->content); 
+			foreach($features as $f){
+			?>
+				<div><?php echo stripslashes($f); ?></div>
+			<?php } ?>				
+		</div>
+	<?php } ?>
+</div>
