21dd6bd40ffa6e5cbeef26726240c90de394c4f1
Chris Hardcastle <me@chrishardcastle.co.uk>
2010-04-22 00:55:41 +0100

    Added the helper reference back into the tweet model to auto link addresses. Needs testing.

--- www/application/models/tweet.php
+++ www/application/models/tweet.php
@@ -16,7 +16,7 @@ class Tweet_Model extends App_Model {
         $tweet = $content["tweet"];
         //preg_replace("#\[(([a-zA-Z]+://)([a-zA-Z0-9?&%.;:/=+_-]*))\]#e", "'<a href=\"$1\" target=\"_blank\">$3</a>'", $content);
         //$tweet = preg_replace("/#[a-zA-Z]+/i", "<a href='http://twitter.com/search?q=urlencode($1)' target='_blank'>$1</a>", $content["tweet"]);
-        $this->tweet = $tweet;
+        $this->tweet = text::auto_link_urls($tweet);
         $this->author = "@hardcastle";
         $this->time = $content["time"];
 		$view = new View("item_{$size}/tweet");
@@ -34,10 +34,7 @@ class Tweet_Model extends App_Model {
 						$profilePic = $link["href"];
 					}
 				}
-				// transform links TODO
-				$tweetText = $tweet["title"];			
-				preg_replace('/\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|](?![^<>]*(?:>|<\/a>))/i', "<a href=\"#\" onclick=\"open_url('\\0')\";return false;>\\0</a>", $tweetText);
-				
+			
 				$content = array(
 					"tweet" => str_ireplace("hardcastle:","",$tweet["title"]),
 					"author "=> $tweet["author"]["name"],
