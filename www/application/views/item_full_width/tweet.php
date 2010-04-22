0f083b332419534ac816c7f6f10b8d83fbbc2e59
Chris Hardcastle <me@chrishardcastle.co.uk>
2010-04-22 23:30:25 +0100

    Made many style ajustments.

--- www/application/views/item_full_width/tweet.php
+++ www/application/views/item_full_width/tweet.php
@@ -5,8 +5,10 @@
 
 ?>
 <div class="tweet fullItem">
+    <b class="b1ftw"></b><b class="b2ftw"></b><b class="b3ftw"></b><b class="b4ftw"></b>
     <div>
         <span class="tweetTime"><?php echo date("H:m",$tweet->time); ?></span>
         <div><?php echo $tweet->tweet; ?></div>
     </div>
+    <b class="b4ftw"></b><b class="b3ftw"></b><b class="b2ftw"></b><b class="b1ftw"></b>
 </div>
--- www/application/views/item_summary/regular.php
+++ www/application/views/item_summary/regular.php
@@ -1,6 +1,8 @@
 <div class="regular item">
+    <b class="b1frg"></b><b class="b2frg"></b><b class="b3frg"></b><b class="b4frg"></b>
     <div>
         <h3><?php echo $article->title; ?></h3>
         <p><?php echo $article->teaser; ?></p>
     </div>
+    <b class="b4frg"></b><b class="b3frg"></b><b class="b2frg"></b><b class="b1frg"></b>
 </div>
