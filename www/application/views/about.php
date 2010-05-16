d677c34524309069afb5e1426acf4847b9435316
Chris Hardcastle <me@chrishardcastle.co.uk>
2010-05-16 22:20:37 +0100

    Fixed about page in welcome controller, still problems with "direct access"

--- www/application/views/about.php
+++ www/application/views/about.php
@@ -1,7 +1,7 @@
-<h4>>Content</h4>
+<h4>Content</h4>
 <p>Most of what you see on this site has been collected from other resources.
 These are other website services that I use such as <a href="http://twitter.com">Twitter</a> and <a href="http://github.com/chardcastle">GitHub</a>. Jump in and engage!</p>
-<h4>>Technology</h4>
+<h4>Technology</h4>
 <p>I've built this site using the Kohana PHP framework. I had originally planned to use Expression Engine,
  the first version of which was great. However I missed the flexibility of a framework and I don't expect
  to be publishing on a scale that requires a content management system.</p>
