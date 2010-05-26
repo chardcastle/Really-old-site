3d8fb2869dd351e59b201209488263a5ec3b2ebf
Chris Hardcastle <me@chrishardcastle.co.uk>
2010-05-26 21:31:52 +0100

    Updated the disclaimer.

--- www/application/views/disclaimer.php
+++ www/application/views/disclaimer.php
@@ -1,5 +1,5 @@
 <h4 class="pageTitle">Disclaimer</h4>
-<p>None of the views or opinions you see on this site reflect those of my employer, past or present. I'm not responsible for any content from any external link within this site. Any resemblemce to other peoples designs, content or any other property is completely co-incidental and unitentional. Any material that is used, I'll give credit where possible. Lots of PHP and Javascript was abused during the creation of this site.</p>
+<p>None of the views or opinions you see on this site reflect those of my employer, past or present. I'm not responsible for any content from any external link within this site. Any resemblemce to other peoples designs, content or any other property is completely co-incidental and unitentional. Any material that is used, I'll give credit where possible. I don't intend to re-use any comments left by visitors on the site. Other than Google analytics, none of your information is used for tracking purposes.</p>
 <p>&nbsp;</p>
 <p>For any further enquiries, please <a href="/page/contact">contact</a> me.</p>
 <p>&nbsp;</p>
